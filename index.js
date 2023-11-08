138d0df26bb22569ff42ff345475cd32e21d701c
Md Ahmar Hashmi <ahmar.hashmi1991@gmail.com>
2023-11-08 18:01:30 +0530

    Pressure check

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const initCanvas = (id) => {
   // Set some options...
   brush.width = 10;
   brush.color = "#000";
-  brush.disableTouch = true; // disable touch and only use mouse and pen devices
+  // brush.disableTouch = true; // disable touch and only use mouse and pen devices
   brush.pressureManager.fallback = 0.3; // fallback value for mouse and touch events
 
   return canvas;
